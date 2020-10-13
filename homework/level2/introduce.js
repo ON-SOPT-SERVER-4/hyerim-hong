72508db2a236a2b7aadfd3561a23cf73104a0745
dudgn3tp <hhr327@naver.com>
2020-10-13 16:04:34 +0900

    level2: done

---
+++ homework/level2/introduce.js
@@ -0,0 +1,12 @@
+const introduce = require('./members');
+const members = introduce.members;
+
+const showMember = (members) =>{
+    console.log("** 서버파트 4조 팀원 소개하기 **\n");
+    members.forEach( members => {
+        const{ name, address, age, hobby } = members;
+        console.log(`이름: ${name}\n거주지: ${address}\n나이: ${age}\n취미: ${hobby}\n\n`);
+    })
+};
+
+showMember(members);
---
+++ homework/level2/members.js
@@ -0,0 +1,46 @@
+const members = [
+    {
+        name: "홍혜림",
+        address: "오리",
+        age: 23,
+        hobby: "집콕하기"
+    },
+    {
+        name: "이영은",
+        address: "건대입구",
+        age: 24,
+        hobby: "운동"
+    },
+    {
+        name: "정효원",
+        address: "성신여대입구",
+        age: 24,
+        hobby: "친구 만나기"
+    },
+    {
+        name: "류세화",
+        address: "교대",
+        age: 23,
+        hobby: "유튜브 보기"
+    },
+    {
+        name: "김우영",
+        address: "서울대입구",
+        age: 24,
+        hobby: "피아노 치기"
+    },
+    {
+        name: "송정우",
+        address: "분당",
+        age: 23,
+        hobby: "농구 보기"
+    },
+    {
+        name: "이현종",
+        address: "인천",
+        age: 25,
+        hobby: "얘기 들어주기"
+    }
+];
+
+exports.members = members;
